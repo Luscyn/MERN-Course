f060b9165e467dc7b54cc8a67aacfe02e2d6e97c
Luscyn <imcayanan.creatives@gmail.com>
2025-07-13 03:08:58 +0800

    Setting up rate limiting and modifying server to connect to DB first before running the app

--- server/src/server.js
+++ server/src/server.js
@@ -2,32 +2,43 @@ import express from 'express'
 import dotenv from 'dotenv'
 import noteRoutes from "./routes/notesRoutes.js"
 import { connectDB } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5001;
 
 const app = express()
-connectDB();
 
-app.use(express.json()) // Middleware to convert req to json
+
+// Middleware
+app.use(express.json()); // Middleware to convert req to json
+app.use(rateLimiter);
+
+// Our custom middleware
+// app.use((req, res, next)=> {
+//     console.log("We just got a new req");
+//     next();
+// })
+
+
 app.get('/', (req, res) => {
-    res.status(200).send("Homepage");
+    res.status(200).send("Dashboard")
 })
-
-
 
 // Routes
 app.use("/api/notes", noteRoutes)
-
 
 // 404
 app.use((req,res)=> {
     res.status(404).send("This page doest exist")
 })
 
+// First connect to db then run server
+connectDB().then(()=>{
+    app.listen(PORT, ()=> {
+        console.log(`Server running at http://localhost:${PORT}`)
+    })
+});
 
-app.listen(PORT, ()=> {
-    console.log(`Server running at http://localhost:${PORT}`)
-})
 
