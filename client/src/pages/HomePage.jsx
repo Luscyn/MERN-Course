a59ce392655ac590cf26a89b7ed4c38fd422658a
Luscyn <imcayanan.creatives@gmail.com>
2025-07-14 15:46:18 +0800

    Setup update functionality

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -45,7 +45,7 @@ const HomePage = () => {
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
 
-        {notes.length === 0 && !isRateLimited && (
+        {notes.length === 0 && !isRateLimited && !loading && (
           <NotesNotFound />
         )}
 
